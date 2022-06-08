32a9fa716a88921ec62dd9894fb4b01b26e9eb16
edwardalex <olooedwards69@gmail.com>
2022-06-08 05:06:24 -0700

    updates

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    aux = 0
-    roman_dict =\
-        {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-    if type(roman_string) != str or roman_string is None:
-        return 0
-    for i in range(len(roman_string)):
-        for j in roman_dict:
-            if i > 0:
-                if roman_string[i] == j \
-                        and roman_dict[roman_string[i - 1]] >= roman_dict[j]:
-                    aux += roman_dict[j]
-                elif roman_string[i] == j \
-                        and roman_dict[roman_string[i - 1]] < roman_dict[j]:
-                    aux -= roman_dict[roman_string[i - 1]]
-                    aux -= roman_dict[roman_string[i - 1]]
-                    aux += roman_dict[j]
-            elif roman_string[i] == j:
-                aux += roman_dict[j]
-    return 
+
+def roman_to_int(roman_string):
+    if roman_string and isinstance(roman_string, str):
+        rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        r_s = roman_string
+        numero = 0
+        for l in range(len(r_s)):
+            if l > 0 and (rom[r_s[l]] > rom[r_s[l - 1]]):
+                numero += rom[r_s[l]] - (2 * rom[roman_string[l - 1]])
+            else:
+                numero += rom[r_s[l]]
+        return numero
+    return (0)
