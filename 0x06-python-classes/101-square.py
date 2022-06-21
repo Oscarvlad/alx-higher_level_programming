0fc400326d75703c5ff8d8c47309aac910d778ae
edwardalex <olooedwards69@gmail.com>
2022-06-21 03:02:09 -0700

    fix

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,76 +1,61 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
-        if type(self.__position) != tuple or len(self.__position) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif any(type(v) != int for v in self.__position):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif any(v < 0 for v in self.__position):
-            raise TypeError("position must be a tuple of 2 positive integers")
+"""Coordinates of a square"""
 
-        if not isinstance(self.__size, int):
-            raise TypeError("size must be an integer")
-        elif self.__size < 0:
-            raise ValueError("size must be >= 0")
+
+class Square:
+    """Private instance attribute: size
+    Instantiation with area and position method """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes attribute size """
+        self.size = size
+        self.position = position
 
     def area(self):
-        return self.__size ** 2
+        """Calculate area of square"""
+        return (self.__size * self.__size)
 
     @property
     def size(self):
+        """Getter for square"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        """Initializes attribute size """
+        if (type(value) is not int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        elif isinstance(value, int):
-            self.__size = value
+        self.__size = value
 
     @property
     def position(self):
+        """Getter for position"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        if type(value) != tuple or len(value) != 2:
+        """Initializes attribute position"""
+        if len(value) is not 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif any(type(v) != int for v in value):
+        if (type(value[0]) is not int or value[0] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif any(v < 0 for v in value):
+        if (type(value[1]) is not int or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def my_print(self):
-        if self.__size != 0:
-            for k in range(self.__position[1]):
-                print()
-
-            for i in range(self.__size):
-                for l in range(self.__position[0]):
-                    print(" ", end="")
-                for j in range(self.__size):
-                    print("#", end="")
-                print()
-        else:
-            print()
+        """Print method"""
+        print(self.__str__())
 
     def __str__(self):
-        s = ""
-        if self.__size != 0:
-            for k in range(self.__position[1]):
-                s += "\n"
-
-            for i in range(self.__size):
-                for l in range(self.__position[0]):
-                    s += " "
-                for j in range(self.__size):
-                    s += "#"
-                s += "\n"
+        """Print representation of squares"""
+        if self.size == 0:
+            return
         else:
-            s += "\n"
-        return s[:-1]
+            str = '\n' * self.__position[1]
+        for i in range(self.__size):
+            str += ' ' * self.position[0]
+            str += '#' * self.__size + '\n'
+        return str[:-1]
