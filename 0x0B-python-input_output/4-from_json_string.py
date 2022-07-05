c509b5d55f952b2c3944e62d3dc266658e4d386f
edwardalex <olooedwards69@gmail.com>
2022-07-05 05:43:06 -0700

    fix

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
-"""This module contains a function that
-returns an object (Python data structure)
-represented by a JSON string
-"""
-
-
+'''Module for to_json_string method.'''
 import json
-"""Import the json module"""
 
 
-def from_json_string(my_str):
-    """ Arguments:
-        my_str: JSON string
-        Return: obj
-    """
-    return json.loads(my_str
-
-            dsddsssdssdsds
+def to_json_string(my_obj):
+    '''Method for rendering object to json string.'''
+    return json.dumps(my_obj)
