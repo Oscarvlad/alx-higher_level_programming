ea77ab739cf7e5cc1d6066ec077e8939b4a23783
edwardalex <olooedwards69@gmail.com>
2022-07-05 05:49:48 -0700

    fix

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,23 +1,41 @@
 #!/usr/bin/python3
-"""This module contains Student
-that defines a student by:
-"""
+"""This module contain a class called Student"""
 
 
 class Student:
-    """This class defines a student"""
+    """Class student"""
     def __init__(self, first_name, last_name, age):
-        """Arguments:
-            firs_name - first name of the student
-            last_name - last name of the student
-            age - age of the student
+        """__init__ method that initialize an instance
+        Args:
+            first_name ([str]): is the first name
+            last_name ([str]): is the second name
+            age ([str, int]): is the age
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """to_json method to retreive dictionary representation"""
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        """to_json method to retreive the dictionary representation
+        that are in attrs
+        Args:
+            attrs ([list], optional): list of strings. Defaults to None.
         """
-            retrieves a dictionary representation of a Student instance
-        """
-        return self.
+        flag = False
+        new = {}
+        if type(attrs) != list or attrs is None:
+            return self.__dict__
+        else:
+            for i in attrs:
+                if type(i) != str:
+                    flag = True
+                    break
+                if i in self.__dict__:
+                    new[i] = self.__dict__[i]
+            if flag:
+                return self.__dict__
+            return new
