eb1e08850eb21f8fa9eafd47937d0fb80e28f26b
edwardalex <olooedwards69@gmail.com>
2022-09-09 11:43:27 -0700

    fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
 """
-    Prevent SQL Injection use "execute" with %s
+    Script that takes in arguments and displays all values in the
+    states table of hbtn_0e_0_usa where name matches the argument.
+    But this time, write one that is safe from MySQL injections!
 """
 
 
-import MySQLdb
 from sys import argv
+import MySQLdb
 
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306, user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
+def main():
+    db = MySQLdb.connect('localhost', argv[1], argv[2], argv[3])
     cur = db.cursor()
-
-    sql = """
-    SELECT id, name FROM states WHERE
-    name COLLATE latin1_general_cs LIKE
-    %s ORDER BY id ASC;
-    """
-    cur.execute(sql, (argv[4], ))
-
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s;", (argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    cur.close()
-    db.close()
+
+
+if __name__ == "__main__":
+    main()
